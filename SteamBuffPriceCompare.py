1b9cd536541ce9d3ae4f0dd08adbc273be1b9a31
Myooooo <46144728+Myooooo@users.noreply.github.com>
2022-08-24 18:51:00 +0930

    Add files via upload

--- SteamBuffPriceCompare.py
+++ SteamBuffPriceCompare.py
@@ -13,7 +13,7 @@ headers = {
         }
 
 # get current price from Steam
-# 730 for CSGO, 23 for CNY
+# appid=730 for CSGO, currency=23 for CNY
 def getSteamPrice(market_hash_name,appid,currency):
 
     url = 'http://steamcommunity.com/market/priceoverview/?'
@@ -25,18 +25,28 @@ def getSteamPrice(market_hash_name,appid,currency):
 
     url = url + urlencode(parameters)
 
+    # send request and parse response
     try:
         response = json.loads(urllib.request.urlopen(url).read())
     except:
         print('\t发送Steam请求失败')
         return -1
 
+    # read price from json
     if response['success'] == True:
-        lowest_price = response['lowest_price'].replace(',','')
-        #median_price = response['median_price']
-        #volume = response['volume']
+        # check if entries exist
+        if 'lowest_price' not in response:
+            print('\t读取Steam价格失败')
+            return -1
+        # if 'median_price' in response:
+        #     median_price = response['median_price']
+        # if 'volume' in response:
+        #     volume = response['volume']
 
-        #print('\n读取Steam价格成功')
+        # read lowest price
+        lowest_price = response['lowest_price'].replace(',','')
+
+        #print('\t读取Steam价格成功')
         print('\tSteam最低价格:',lowest_price)
         #print('Steam中位价格:',median_price)
         #print('Steam市场存量:',volume)
@@ -48,32 +58,48 @@ def getSteamPrice(market_hash_name,appid,currency):
 
         return lowest_price
     else:
-        print('\n读取Steam价格失败')
+        print('\t读取Steam价格失败')
         return -1
+
+# calculate discount rate percentage
+def processItem(item):
+    steam_lowest_price = getSteamPrice(item['market_hash_name'],'730','23')
+    buff_lowest_price = float(item['sell_min_price'])
+
+    # calculate price difference
+    if steam_lowest_price != -1:
+        received = steam_lowest_price / 1.15
+        per_diff = ( buff_lowest_price / received ) * 100
+        return received,per_diff
+    
+    return 0,100
 
 # search keyword on Buff
 def searchBuff(keyword):
     url = 'https://buff.163.com/api/market/goods?game=csgo&page_num=1&search=' + keyword
     url = quote(url,safe=string.printable)
-    request = urllib.request.Request(url,headers=headers)
 
+    # send request and read response
     try:
-        response = urllib.request.urlopen(request).read()
+        response = urllib.request.Request(url,headers=headers)
+        response = urllib.request.urlopen(response).read()
     except:
         print('\n发送Buff请求失败,请检查网络')
         return -1
 
+    # write cache
     file_path = os.path.join(os.path.dirname(__file__),'./cache/')
     file_name = keyword + ".json"
     
     try:
         if not os.path.exists(os.path.realpath(file_path)):
             os.mkdir(os.path.realpath(file_path))
-        fw = open(os.path.realpath(file_path + file_name), "wb")
-        fw.write(response)
+        with open(os.path.realpath(file_path + file_name), "wb") as fw:
+            fw.write(response)
     except IOError:
         print('\n写入缓存失败')
 
+    # parse response
     response_json = json.loads(response)
     
     if response_json['code'] == 'OK':
@@ -89,30 +115,25 @@ def searchBuff(keyword):
     result_count = len(response_json['data']['items'])
     print('找到%d个结果\n' %result_count)
 
+    
+    # process items
     for i in range(result_count):
         item = response_json['data']['items'][i]
         print('\n[%d] %s\n' %((i+1),item['market_hash_name']))
-        processItem(item)
-        time.sleep(0.5)
+        received,per_diff = processItem(item)
 
-    fw.close()
+        print('\tBuff最低价格: ¥',item['sell_min_price'])
+        #print('市场存量:',item['sell_num'])
+        #print('最高求购价格: ¥',item['buy_max_price'])
+        #print('求购数量:',item['buy_num'])
+        print('\t到手余额: ¥%.2f' %received)
+        print('\t差价百分比: %.2f%%' %per_diff)
+
+        # pause to avoid ban
+        time.sleep(0.5)
+    
     print('\n搜索完成\n')
     return 0
-
-# calculate discount rate percentage
-def processItem(item):
-    steam_lowest_price = getSteamPrice(item['market_hash_name'],'730','23')
-
-    print('\tBuff最低价格: ¥',item['sell_min_price'])
-    #print('市场存量:',item['sell_num'])
-    #print('最高求购价格: ¥',item['buy_max_price'])
-    #print('求购数量:',item['buy_num'])
-
-    buff_lowest_price = float(item['sell_min_price'])
-
-    if steam_lowest_price != -1:
-        per_diff = ( buff_lowest_price / (steam_lowest_price / 1.15) ) * 100
-        print('\t差价百分比(包含手续费): %.2f%%' %per_diff)
     
 if __name__ == '__main__':
     keyword = input('搜索饰品：')
